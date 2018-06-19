c56cd7bb66d8b37c2347722ffcaea87bf4baf1d0
robin.mehra <robin.mehra@tothenew.com>
2018-06-19 12:36:45 +0530

    modal folder deleted

---
+++ dist/precache-manifest.98133af29c0504f66a33ddf2bc6ef20b.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "176e1b1e57a99dc0732f",
+    "url": "/distvendor.js"
+  },
+  {
+    "revision": "8600c2290dc668578619",
+    "url": "/distindex.js"
+  }
+];
