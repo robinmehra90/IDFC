f2d69b49f22f60cc18ee46548cda10c20508e8e0
robin.mehra <robin.mehra@tothenew.com>
2018-06-20 17:38:19 +0530

    header for admin

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,35 @@ import './styles.scss';
 
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            adminAuth: true
+        }
+    }
+
     render () {
+        const rightNav =
+            this.state.adminAuth ?
+                <div className="text-align-left header-right-nav">
+                    <ul>
+                        <li>Employee Master</li>
+                        <li>Tool Master</li>
+                        <li>Review Orders</li>
+                        {/*make a common dropdown*/}
+                        <li className="pull-right">
+                            <label>Hi Admin</label>
+                            <a href="#"><i className="icon-login" />Logout</a>
+                        </li>
+                    </ul>
+                </div>
+                :
+                <div className="header-right-nav">
+                    <ul>
+                        <li><Link to="/"><i className="icon-cart" /> Cart</Link></li>
+                        <li><a href="#"><i className="icon-login" />Login</a></li>
+                    </ul>
+                </div>;
         return (
             <header className="header">
                 <div className="container">
@@ -13,13 +41,7 @@ export default class Header extends Component {
                             <img src="./public/images/logo.png" alt="logo" />
                         </a>
                     </div>
-                    <div className="header-right-nav">
-                        <ul>
-                            <li><Link to="/"><i className="icon-cart" /> Cart</Link></li>
-                            <li><a href="#"><i className="icon-login" />Login</a></li>
-                        </ul>
-                    </div>
-
+                    {rightNav}
                 </div>
             </header>
         )
