f2383f08fb8c5041b76df3b9a360fbe67e31e60c
robin.mehra <robin.mehra@tothenew.com>
2018-06-20 16:20:01 +0530

    react-tabs

--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class Admin extends Component {
     constructor(props) {
@@ -18,6 +19,19 @@ export default class Admin extends Component {
         return(
             <div className="admin-wrapper">
                 Admin
+                <Tabs>
+                    <TabList>
+                        <Tab>Title 1</Tab>
+                        <Tab>Title 2</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <h2>Any content 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Any content 2</h2>
+                    </TabPanel>
+                </Tabs>
             </div>
         )
     }
